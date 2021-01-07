f4a490a1fcd6b38de8cdf63d1e4753c7348d04fe
Dave Rolsky <autarch@urth.org>
2021-01-07 16:06:36 -0600

    Add bootstrap script for unix platforms

---
+++ bootstrap/bootstrap-ubi.sh
@@ -0,0 +1,50 @@
+#!/bin/sh
+
+set -e
+set -x
+
+TAG=$(
+    # From https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
+    curl --silent "https://api.github.com/repos/houseabsolute/ubi/releases/latest" |
+        grep '"tag_name":' |
+        sed -E 's/.*"([^"]+)".*/\1/'
+)
+
+if [ -z "$TAG" ]; then
+    echo "Cannot find a UBI release!"
+    exit 1
+fi
+
+TARGET="$HOME/bin"
+if [ $(id -u) -eq 0 ]; then
+    TARGET="/usr/local/bin"
+fi
+
+chdir "$TARGET"
+
+KERNEL=$(uname -s)
+case "$KERNEL" in
+    Linux)
+        PLATFORM="Linux"
+        ;;
+    darwin)
+        PLATFORM="Darwin"
+        ;;
+    *)
+        echo "Cannot determine what binary to download for your kernel: $KERNEL"
+        exit 2
+        ;;
+esac
+
+ARCH=$(uname -p)
+case "$ARCH" in
+     x86_64)
+         CPU="x86_64"
+         ;;
+     *)
+         echo "Cannot determine what binary to download for your CPU architecture: $ARCH"
+         exit 3
+esac
+
+curl --silent --location https://github.com/houseabsolute/ubi/releases/download/"$TAG"/ubi-"$PLATFORM"-"$CPU".tar.gz |
+    tar -xzf - ubi 
