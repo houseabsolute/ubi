36ab17f63d79ed38880ee0b87e196ba80f76c976
Dave Rolsky <autarch@urth.org>
2021-10-20 12:28:06 -0500

    Really, finally (I hope) fix issues with GH token in bootstrap script

--- bootstrap/bootstrap-ubi.sh
+++ bootstrap/bootstrap-ubi.sh
@@ -2,12 +2,21 @@
 
 set -e
 
+# We need a space in the Authorization header, which means we need to quote it
+# in the curl command later, as "$AUTH". That means we can't put the
+# `--header` flag in this variable, because if we do then the entire string of
+# `--header Authorization:token ...` is interpreted as a single unit by the
+# shell, and curl is confused. So we need to hardcode the `--header` in the
+# command later. But that means we have to have _something_ to put after
+# `--header`, even if it's nonsense.
 if [ -n "$GITHUB_TOKEN" ]; then
-    AUTH="--header Authorization:token%20$GITHUB_TOKEN"
+    AUTH="Authorization:token $GITHUB_TOKEN"
+else
+    AUTH="X-Cannot-Be-Empty"
 fi
 
 URI="https://api.github.com/repos/houseabsolute/ubi/releases/latest"
-RELEASES=$(curl $AUTH --show-error --silent $URI)
+RELEASES=$(curl --header "$AUTH" --show-error --silent $URI)
 if [ -z "$RELEASES" ]; then
     >&2 echo "Did not get a response body back from $URI"
     exit 1
