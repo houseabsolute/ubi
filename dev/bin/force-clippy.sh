a00cd90d681e87edb4e5f7f47c78de5fa1033940
Dave Rolsky <autarch@urth.org>
2021-10-27 11:41:03 -0500

    Remove force-clippy.sh and just use cargo clippy
    The issue with clippy not re-running has been fixed for a while.

--- dev/bin/force-clippy.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -e
-
-# This seems to be just enough to force a recompilation, so clippy is actually
-# executed. But it doesn't require rebuilding every dep, so it's pretty fast.
-rm -fr target/debug/deps/*ubi*
-cargo clippy --all-targets --all-features -- -D clippy::all
