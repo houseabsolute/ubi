804edb210d038ccf4583b03d2fec599af0974303
Dave Rolsky <autarch@urth.org>
2023-05-20 11:24:09 -0500

    Change the bootstrap scripts to use /releases/latest/download links
    This means that the bootstrap scripts no longer need to hit the GitHub API to
    download ubi.

--- bootstrap/bootstrap-ubi.ps1
+++ bootstrap/bootstrap-ubi.ps1
@@ -1,15 +1,10 @@
-## Without -UseBasicParsing I had an error on my test VM about IE not being
-## fully installed, and this seems to work fine with this flag.
-$DlFileURL = (Invoke-WebRequest -URI "https://github.com/houseabsolute/ubi/releases/latest/" -UseBasicParsing).BaseResponse.ResponseUri.OriginalString
+$Filename = "ubi-Windows-x86_64.zip"
 
-$DlFileName = "ubi-Windows-x86_64.zip"
+$URL = "https://github.com/houseabsolute/ubi/releases/latest/download/$($Filename)"
 
-$DlFileURL = "$DlFileURL" -replace '/tag/','/download/'
-$DlFileURL = "$DlFileURL/$DlFileName"
+$TempFile = "$env:TEMP/$Filename"
 
-$TempFile = "$env:TEMP/$DlFileName"
-
-Invoke-WebRequest -URI "$DlFileURL" -OutFile "$TempFile"
+Invoke-WebRequest -URI "$URL" -OutFile "$TempFile"
 
 Expand-Archive -Path "$TempFile" -DestinationPath "$env:TEMP" -Force
 
--- bootstrap/bootstrap-ubi.sh
+++ bootstrap/bootstrap-ubi.sh
@@ -2,45 +2,6 @@
 
 set -e
 
-# We need a space in the Authorization header, which means we need to quote it
-# in the curl command later, as "$AUTH". That means we can't put the
-# `--header` flag in this variable, because if we do then the entire string of
-# `--header Authorization:token ...` is interpreted as a single unit by the
-# shell, and curl is confused. So we need to hardcode the `--header` in the
-# command later. But that means we have to have _something_ to put after
-# `--header`, even if it's nonsense.
-echo ""
-if [ -n "$GITHUB_TOKEN" ]; then
-    echo "Setting Authorization header using GITHUB_TOKEN env var"
-    AUTH="Authorization:token $GITHUB_TOKEN"
-else
-    echo "GITHUB_TOKEN env var is not set"
-    AUTH="X-Cannot-Be-Empty"
-fi
-
-if [ -z "$TAG" ]; then
-    URI="https://api.github.com/repos/houseabsolute/ubi/releases/latest"
-    RELEASES=$(curl --header "$AUTH" --show-error --silent $URI)
-    if [ -z "$RELEASES" ]; then
-        >&2 echo "Did not get a response body back from $URI"
-        exit 1
-    fi
-
-    TAG=$(
-        # From https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
-        echo "$RELEASES" |
-            grep '"tag_name":' |
-            sed -E 's/.*"([^"]+)".*/\1/'
-    )
-
-    if [ -z "$TAG" ]; then
-        >&2 echo "boostrap-ubi.sh: Cannot find a UBI release based on GitHub API response!"
-        >&2 echo ""
-        >&2 echo "$RELEASES"
-        exit 2
-    fi
-fi
-
 if [ $(id -u) -eq 0 ]; then
     DEFAULT_TARGET="/usr/local/bin"
 else
@@ -94,7 +55,11 @@ case "$ARCH" in
 esac
 
 FILENAME="ubi-$PLATFORM-$CPU$ABI.$EXT"
-URL="https://github.com/houseabsolute/ubi/releases/download/$TAG/$FILENAME"
+if [ -z "$TAG" ]; then
+    URL="https://github.com/houseabsolute/ubi/releases/latest/download/$FILENAME"
+else
+    URL="https://github.com/houseabsolute/ubi/releases/download/$TAG/$FILENAME"
+fi
 
 TEMPDIR=$( mktemp -d )
 trap 'rm -rf -- "$TEMPDIR"' EXIT
