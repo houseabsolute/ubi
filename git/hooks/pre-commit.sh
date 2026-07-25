f78d6cd0a9c365b20f19ce99295ee16400ee2cbe
Dave Rolsky <autarch@urth.org>
2026-07-25 12:21:05 -0500

    Change the pre-commit hook to use `just`

--- git/hooks/pre-commit.sh
+++ git/hooks/pre-commit.sh
@@ -1,15 +1,5 @@
 #!/bin/bash
 
-declare -i status
-status=0
+set -e
 
-PRECIOUS=$(which precious)
-if [[ -z $PRECIOUS ]]; then
-    PRECIOUS=./bin/precious
-fi
-
-if ! "$PRECIOUS" lint -s; then
-    status+=1
-fi
-
-exit $status
+just lint -s
